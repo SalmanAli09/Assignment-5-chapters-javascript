c45c8529c937641ba900994fc0df36f6f89d68db
SalmanAli09 <64953911+SalmanAli09@users.noreply.github.com>
2020-06-14 05:02:15 -0800

    assgnment-5

---
+++ app.js
@@ -0,0 +1,387 @@
+// ======> CHAPTER # 1 <======
+// ======> TASK # 1 <======
+
+// alert("Hello World")
+
+//  ======> TASK # 2 <======
+
+// alert("Error!PLease enter a valid password.")
+
+// ======>  TASK # 3 <======
+
+// alert("Welcome to JS land ... \nHappy Coding!") 
+
+// ======> TASK # 4 <======
+
+// alert("Welcome to JS Land...")
+// alert("Happy Coding!")
+
+//  ======> TASK # 5 <=======
+
+// alert("Hello.. I can run JS through my web browser's console")
+// console.log
+
+
+// ======>  TASK # 6 <======
+
+// Alreay Done
+    //  ========================END======================
+//  ======> CHAPTER # 2  <======
+//  ======> TASK # 1 <======
+
+// var username;
+  
+//  ======> TASK # 2 <=====
+
+// var myName;
+// myName = "Salman Ali";
+
+//  ======> TASK # 3 <=====
+
+// var Message;
+// message = "Hello World !";
+// alert("HelloWorld")
+
+
+//  ======> TASK # 4 <=====
+
+
+// var name = ("Jhon Deo");
+// alert("Jhon Deo")
+// var age  = ("15 Years OLd");
+// alert("15 Years OLd")
+// var qulification  = (" Certified Mobile Application Development");
+// alert("Certified Mobile Application Development")
+
+
+
+//  ======> TASK # 5 <=====
+
+
+// var name = ("pizza");
+// alert("pizza\npizz\npiz\npi\np")
+
+//  ======> TASK # 6 <=====
+
+
+// var email;
+// alert("My E-Mail Address is example@example.com")
+
+//  ======> TASK # 7 <=====
+
+// var book;
+// book = ("A smarter way to learn JavaScrip")
+// alert("I am trying to learn from the book A Smarter Way to learn Javascript")
+  
+
+//  ======> TASK # 8 <=====
+
+
+//  var a = ("write using document.write ")
+//  document.write("Yah! I can write HTML content using Java script")
+
+
+//  ======> TASK # 9 <=====
+
+
+// alert("▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬")
+
+// ========================END======================
+
+// ==========>  CHAPTER # 3  <==========
+
+//       ======> TASK # 1 <======
+
+
+ // var age;
+// age = "15 years old";
+// alert("Iam 15 years old.")
+
+        // ======> TASK # 2 <======
+
+
+
+
+
+
+
+        // ======> TASK # 3 <======
+
+
+// var BirthYear;
+// BirthYear = ("2003")
+// document.write("My BirthYear is" + " " + "2003") 
+
+    
+        // ======> TASK # 4 <======
+
+        
+// var name = ("Jhon Deo");
+// var title = ("XYZCLOTHGING Store");
+// var quantity = ("5 T-shirts");
+//  document.write("Jhon Deo ordered 5 T-shirtson XYZ.CLOTHING STORE")      
+  
+
+
+           //  ========================END======================
+
+
+         // ==========>  CHAPTER # 4  <==========
+
+            
+         //       ======> TASK # 1 <======
+           
+
+        //  var variable1 = "Hello World!";
+        //  var variable2 = "Testing...";
+        //  var variable3 = "42";      
+
+        //  alert("variable1,variable2,variable3")
+
+        //       ======> TASK # 2 <======
+
+// var name = ("legal variable");
+// var ali1;
+// var $mobile;
+// var nokia12;
+// var x1;
+// var b01;  
+// // var name = ("Illegal variable");
+// // var 1ali;
+// // var ?mobile;
+// // var %a ;
+// // var *blur;
+
+
+
+            //    ======> TASK # 3 <======
+    
+// document.write("<h1> “Rules for naming JS variables” </h1>")
+
+// document.write("<h4>Variable names can only contain ______, ______, ______ and ______. For example $my_1stVariable </h4>");
+// document.write("<h4>Variables must begin with a ______, ______ or _____. For example $name, _name or nam</h4>")
+// document.write("<h4> Variable names are case _________ </h4>")
+// document.write("<h4>Variable names should not be JS _______</h4>")
+
+
+              
+           //  ========================END======================
+          
+
+           // ==========>  CHAPTER # 5  <==========
+            
+
+            //       ======> TASK # 1 <======
+
+//  var a = 3;
+//  var b = 5;
+//  var c = a + b;
+//  document.write("The Sum Of These Numbers are" + " " + (a + b))
+ 
+              //       ======> TASK # 2 <======
+   
+
+// var subtration;
+//  var a = 3;
+//  var b = 5;
+//  var c = a - b;
+//  document.write("The subtration Of These Numbers are" + " " + (a - b))
+
+
+// var multiplication;
+//  var a = 3;
+//  var b = 5;
+//  var c = a * b;
+//  document.write("The multiplication Of These Numbers are" + " " + (a * b))
+
+// var division;
+//  var a = 3;
+//  var b = 5;
+//  var c = a / b;
+//  document.write("The division Of These Numbers are" + " " + (a / b))
+
+// var modulus;
+//  var a = 3;
+//  var b = 5;
+//  var c = a % b;
+//  document.write("The modulus Of These Numbers are" + " " + (a % b))
+
+             //       ======> TASK # 3 <======
+
+// var a = 10;
+
+// document.write("“Value after variable declaration is: 1")
+
+
+// var a = 5;
+// var a;
+// a = 5;
+
+// document.write("Initial value : 5")
+
+// var a = 5;
+// var a;
+// a = 5;
+// var b  = ++a
+ 
+// var a = 5;
+// var a;
+// a = 5;
+// var b  = ++a
+// document.write("“Value after increment is: 6")
+
+// var a = 6;
+// var a;
+// a = 6;
+// var b  = (a + 7)
+// console.log(b)
+ 
+
+// var a = 6;
+// var a;
+// a = 6;
+// var b  = (a + 7)
+// document.write("Value  after addition is" + " " + (a + 7))
+
+
+// var a = 13;
+// var a;
+// a = 13;
+// var b  = --a
+// console.log(b)
+
+// var a = 13;
+// var a;
+// a = 13;
+// var b  = a--
+// document.write("Value  after decrement  is" + " " + (a--))
+
+// var a = 12;
+// var a;
+// a = 12;
+// var b  = a%3
+// console.log(b)
+  
+
+// var a = 12;
+// var a;
+// a = 12;
+// var b  = a%3
+// document.write("Value  after decrement  is" + " " + (a%3))
+
+          //       ======> TASK # 4 <======
+
+// var a = 600;
+// var b = 5;
+// var c = a*b
+// document.write("Total cost to buy 5 ticket for the movie is" + " " + (a*b))
+
+            //       ======> TASK # 5  <======
+
+
+// var i, j;
+// for (i = 1; i <= 1; i++) {
+// document.write("Table" + " " + i + "<br>");
+// for (j = 1; j <= 10; j++)
+// document.write(i + "*" + j + "=" + i * j + "<br>");
+// }
+ 
+            //       ======> TASK # 6  <======
+
+//  function cToF(celsius) 
+// {
+//   var cTemp = celsius;
+//   var cToFahr = cTemp * 9 / 5 + 32;
+//   var message = cTemp+'\xB0C is ' + cToFahr + ' \xB0F.';
+//     console.log(message);
+// }
+
+// function fToC(fahrenheit) 
+// {
+//   var fTemp = fahrenheit;
+//   var fToCel = (fTemp - 32) * 5 / 9;
+//   var message = fTemp+'\xB0F is ' + fToCel + '\xB0C.';
+//     console.log(message);
+// } 
+// cToF(60);
+// fToC(45);
+
+  //       ======> TASK # 7  <======
+
+
+//  var a = 650;
+//  var b = 100;
+//  var c = 3;
+//  var d = 7;
+//  var e = a*c
+//  var f = b*d
+//  document.write("<h1>SHOPPING CART</h1>")
+//  document.write("Price of item 1 :" + " " + (a) + "<br>")
+//  document.write("Quantity of item 1 :" + " " + (c) + "<br>"  )
+//  document.write("Price of item 2 :" + " " + (b) + "<br>")
+//  document.write("Quantity of item 2 :" + " " + (d) + "<br>"  )
+//  document.write("TOTAL COST :" + " " + (a*c + b*d) + "<br>"  )
+  
+
+                 //       ======> TASK # 8  <======
+
+
+// var a = 980;
+// var b = 804;
+// var c = a/b*100
+// document.write("Total marks:" + " " + (a) + "<br>")
+// document.write("Marks obtained:" + " " + (b) + "<br>"  )
+// document.write("Percentage Obtained :" + " " + (b/a*100 ) + "<br>"  )
+
+
+                //       ======> TASK # 9  <======
+
+
+// var a = 1; 
+// var b = 104.80;
+// var c = 1;
+// var d = 28;
+// var e = b + d
+// document.write("<h1>PAKISTANI CURRENCY PKR</h1>")
+// document.write("TOTAL CURRENCY IN PKR:" + " " + (b + d))
+
+          
+                //       ======> TASK # 10  <======
+
+// (A)
+// var a = 10;
+// var b = 5;
+// var c = a + b
+
+
+//   (B)
+//   var a = 15;
+//   var b = 2;
+//   var c = a*b
+  
+    //  (C)
+    //  var a = 30;
+    //  var b = 2;
+    // var c = a/b
+
+    // (D)
+    //   var a = 10;
+    //   var b = 2;
+    //   var c = 25;
+    //   var d = 30;
+    //   var e = a+b*c/d
+   
+
+          
+                //       ======> TASK # 11   <======
+
+// var a = 2016;
+// var b = 1992;
+// var c = a-b
+// document.write("<h1>CALCULATE YOUR AGE</h1>"  +  "<br>" )
+//  document.write("CURRENT YEAR :" + " " + (a) + "<br>"  )
+//  document.write("DATE OF BIRTH :" + " " + (b) + "<br>"  )
+// document.write("YOUR AGE IS;" + " " + (a - b))
+
+
+        //  ========================END======================
